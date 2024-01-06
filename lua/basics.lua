b847249ec2347dc10e8b27cb9e14fac06244d7ae
yigithanbalci <13695727+yigithanbalci@users.noreply.github.com>
2024-01-06 15:10:32 +0300

    split keymap configs

---
+++ lua/basics.lua
@@ -0,0 +1,12 @@
+vim.opt.scrolloff = 8
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+
+lvim.format_on_save.enabled = true
+lvim.transparent_window = true
