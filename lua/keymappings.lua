bc4d9ad87dc2745f75ab19fcee540f0b12a6650d
Yigithan Kadir BALCI <13695727+yigithanbalci@users.noreply.github.com>
2024-03-09 11:51:52 +0300

    Add DAP mappings for debug session

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -10,3 +10,9 @@ lvim.builtin.which_key.mappings['h'] = {
 
 lvim.keys.visual_mode["J"] = ":m '>+1<CR>gv=gv"
 lvim.keys.visual_mode["K"] = ":m '<-2<CR>gv=gv"
+
+vim.keymap.set("n", "<F1>", ":lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F2>", ":lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F3>", ":lua require'dap'.step_out()<CR>")
+--vim.keymap.set("n", "<F4>", ":lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
